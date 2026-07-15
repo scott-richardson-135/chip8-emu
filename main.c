d9fc1471480043bdfa072176f1d937f485023128
Scott Richardson <srich1550@gmail.com>
2026-07-14 22:48:40 -0600

    rom loading added

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "chip8.h"
 
@@ -28,17 +29,55 @@ void print_state(struct State *state) {
         printf("0x%02X ", state->memory[FONT_START_ADDR + i]);
     }
     printf("\n");
+
+    printf("Rom bytes at 0x200:\n");
+    for (int i = 0; i < 16; i++) {
+        printf("0x%02X ", state->memory[0x200 + i]);
+    }
+    printf("\n");
+
 }
 
 int main(int argc, char *argv[]) {
     
+    if (argc != 2) {
+        printf("Usage: %s <rom_file>\n", argv[0]);
+        return 1;
+    }
+
     /*STATE SETUP*/
     struct State state;
     init_state(&state);
 
 
-    print_state(&state);
-    
+    /*LOAD ROM*/
+    // maybe this needs some better handling if the file isn't right but I don't care right now
+    FILE *rom_file = fopen(argv[1], "rb");
+    if (rom_file == NULL) {
+        perror("Error opening file");
+        return 1;
+    } 
 
+    //find the end of file and calculate how big the file is
+    fseek(rom_file, 0, SEEK_END); 
+    long file_size = ftell(rom_file);
+    rewind(rom_file);
+
+    if (file_size > MEM_SIZE - 0x200) {
+        printf("ROM file too large");
+        return 1;
+    }
+
+    size_t bytes_read = fread(state.memory + 0x200, 1, file_size, rom_file);
+    if (bytes_read != (size_t)file_size) {
+        perror("error reading file");
+        fclose(rom_file);
+        return 1;
+    }
+
+    fclose(rom_file);
+
+    print_state(&state); //debug
+    
     return 0;
 }
